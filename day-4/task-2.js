7a16309e8043e4b1c4704d896aa48f8e5a24bf7a
koscheiundead <koscheiundead@gmail.com>
2023-12-04 10:30:33 -0800

    task-2

--- day-4/task-2.js
+++ day-4/task-2.js
@@ -1,13 +1,13 @@
 const input = require("./input.js");
 
-//total we're looking for:
-
 //how many total scratchcards do we have? 1 copy of 202 individual games
 let scratchcards = {}
 for (let i = 1; i <= 202; i++) {
   scratchcards[i] = 1;
 }
 
+//for each game, understand how many copies we have and how many points each copy is worth
+//then, allocate additional copies out to remaining games accordingly
 for (let i = 1; i <= 202; i++) {
   let copies = scratchcards[i];
   let points = cleanseLine(input[i - 1]);
@@ -18,6 +18,8 @@ for (let i = 1; i <= 202; i++) {
   }
 }
 
+//sum up all cards to determine how many copies we have. it's too many, folks. we have too many lotto cards.
+//i'm worried about you, elf friend.
 let totalCopies = Object.values(scratchcards).reduce((sum, value) => sum + value, 0);
 console.log(totalCopies)
 
